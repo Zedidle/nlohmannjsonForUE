e02c65722424647bd8906db44743f0e591a9c9f4
DESKTOP-TDJFEVS\PQT045 <476409251@qq.com>
2024-04-27 18:56:07 +0800

    补充一些方法

--- Source/nlohmannjson/Private/nlohmannjsonBPLibrary.cpp
+++ Source/nlohmannjson/Private/nlohmannjsonBPLibrary.cpp
@@ -14,13 +14,22 @@ FJSON UnlohmannjsonBPLibrary::loadjsonfile(const FString& file)
 	std::ifstream f(TCHAR_TO_UTF8(*file));
 	if (!f) {
 		// 抛出异常
-		return nullptr;
+		//throw("Can't find file %s", file);
+		//throw std::runtime_error("load json file failure");
+		UE_LOG(LogTemp, Error, TEXT("UnlohmannjsonBPLibrary::loadjsonfile load error: %s"), *file);
+		return FJSON(nullptr);
 	}
 
 	json data = json::parse(f);
 	FJSON j(data);
 	f.close();
 	return  j;
+}
+
+FJSON UnlohmannjsonBPLibrary::Parse(const FString& content)
+{
+	json j = json::parse(TCHAR_TO_UTF8(*content));
+	return FJSON(j);
 }
 
 const FJSON& UnlohmannjsonBPLibrary::PrintJSON(const FJSON& JSON)
@@ -46,18 +55,33 @@ void UnlohmannjsonBPLibrary::PrintJSONType(const FJSON& JSON)
 	}
 }
 
+FString UnlohmannjsonBPLibrary::JSONToString(const FJSON& JSON)
+{
+	string s = JSON.data.dump();
+	const FString Message = FString::Printf(TEXT("%s"), UTF8_TO_TCHAR(s.c_str()));
+	return Message;
+}
+
 FString UnlohmannjsonBPLibrary::GetString(const FJSON& JSON, const FString& str)
 {
 	string key = TCHAR_TO_UTF8(*str);
-	json b = JSON.data[key];
-	check(b.is_string());
-	if (b.is_string()) {
-		string result = b.template get<string>();
-		return result.c_str();
+	if (JSON.data.contains(key))
+	{
+		json b = JSON.data[key];
+
+		check(b.is_string());
+		if (b.is_string()) {
+			string result = b.template get<string>();
+			return result.c_str();
+		}
+		else {
+			UE_LOG(LogTemp, Log, TEXT("JSON GetString :  No String Type ."));
+			return "JSONGetString-NoStringType";
+		}
 	}
-	else {
-		UE_LOG(LogTemp, Log, TEXT("JSON GetString :  No String Type ."));
-		return "JSONGetString-NoStringType";
+	else
+	{
+		return "Key-Absent";
 	}
 }
 
@@ -89,6 +113,7 @@ int32 UnlohmannjsonBPLibrary::GetInteger(const FJSON& JSON, const FString& str)
 		UE_LOG(LogTemp, Log, TEXT("JSON GetFloat :  No Integer Type ."));
 		return 0;
 	}
+
 }
 
 bool UnlohmannjsonBPLibrary::GetBoolean(const FJSON& JSON, const FString& str)
@@ -104,21 +129,54 @@ bool UnlohmannjsonBPLibrary::GetBoolean(const FJSON& JSON, const FString& str)
 		UE_LOG(LogTemp, Log, TEXT("JSON GetFloat :  No Boolean Type ."));
 		return false;
 	}
+
+}
+
+bool UnlohmannjsonBPLibrary::IsObjectSelf(const FJSON& JSON)
+{
+	return JSON.data.is_object();
+}
+
+bool UnlohmannjsonBPLibrary::IsObject(const FJSON& JSON, const FString& str)
+{
+	string key = TCHAR_TO_UTF8(*str);
+	json b = JSON.data[key];
+	return b.is_object();
 }
 
 FJSON UnlohmannjsonBPLibrary::GetObject(const FJSON& JSON, const FString& str)
 {
 	string key = TCHAR_TO_UTF8(*str);
+	if (JSON.data.contains(key))
+	{
+		json b = JSON.data[key];
+		check(b.is_object());
+		if (b.is_object()) {
+			json result = b.template get<json>();
+			return FJSON(result);
+		}
+		else {
+			UE_LOG(LogTemp, Log, TEXT("JSON GetObject :  No Object Type ."));
+			return FJSON();
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Log, TEXT("JSON GetObject :  Key-Absent."));
+		return FJSON();
+	}
+}
+
+bool UnlohmannjsonBPLibrary::IsArraySelf(const FJSON& JSON)
+{
+	return JSON.data.is_array();
+}
+
+bool UnlohmannjsonBPLibrary::IsArray(const FJSON& JSON, const FString& str)
+{
+	string key = TCHAR_TO_UTF8(*str);
 	json b = JSON.data[key];
-	check(b.is_object());
-	if (b.is_object()) {
-		json result = b.template get<json>();
-		return FJSON(result);
-	}
-	else {
-		UE_LOG(LogTemp, Log, TEXT("JSON GetFloat :  No Object Type ."));
-		return nullptr;
-	}
+	return b.is_array();
 }
 
 FJSON UnlohmannjsonBPLibrary::GetArray(const FJSON& JSON, const FString& str)
@@ -132,7 +190,7 @@ FJSON UnlohmannjsonBPLibrary::GetArray(const FJSON& JSON, const FString& str)
 	}
 	else {
 		UE_LOG(LogTemp, Log, TEXT("JSON GetFloat :  No Array Type ."));
-		return nullptr;
+		return FJSON();
 	}
 }
 
@@ -145,7 +203,20 @@ FJSON UnlohmannjsonBPLibrary::GetArrayElement(const FJSON& JSON, int32 Index)
 		return FJSON(b);
 	}
 	else {
-		return nullptr;
+		return FJSON();
+	}
+}
+
+int UnlohmannjsonBPLibrary::GetArraySize(const FJSON& JSON)
+{
+	check(JSON.data.is_array());
+	if (JSON.data.is_array())
+	{
+		return JSON.data.size();
+	}
+	else
+	{
+		return 0;
 	}
 }
 
@@ -201,3 +272,35 @@ bool UnlohmannjsonBPLibrary::ToBoolean(const FJSON& JSON)
 	}
 }
 
+FString UnlohmannjsonBPLibrary::UrlEncode(const FString& String)
+{
+	string str = TCHAR_TO_UTF8(*String);
+	std::ostringstream escaped;
+	escaped.fill('0');
+	escaped << std::hex;
+
+	for (char c : str) {
+		// 保留字符不编码
+		if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~') {
+			escaped << c;
+		}
+		else {
+			// 将非保留字符编码为%xx形式
+			escaped << '%' << std::setw(2) << int((unsigned char)c);
+		}
+	}
+
+	//UE_LOG(LogTemp, Log, TEXT("UrlEncode2 : %s ."), escaped.str().c_str());
+	return escaped.str().c_str();
+}
+
+FString UnlohmannjsonBPLibrary::BinaryDecode(const FString& Message)
+{
+		// 在这里处理来自 JavaScript 的消息
+		// 你需要将 UTF-8 编码的字符串转换为 Unreal Engine 的 FString
+		FString DecodedMessage = FString(UTF8_TO_TCHAR(TCHAR_TO_UTF8(*Message)));
+
+		// 现在你可以在这里处理你的中文消息
+		UE_LOG(LogTemp, Log, TEXT("Received message from JavaScript: %s"), *DecodedMessage);
+		return DecodedMessage;
+}
--- Source/nlohmannjson/Public/nlohmannjsonBPLibrary.h
+++ Source/nlohmannjson/Public/nlohmannjsonBPLibrary.h
@@ -5,6 +5,9 @@
 #include <nlohmann/json.hpp>
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "fstream"
+#include <iostream>
+#include <iomanip>
+#include <sstream>
 #include "nlohmannjsonBPLibrary.generated.h"
 
 using namespace std;
@@ -18,25 +21,32 @@ struct FJSON
 	FJSON(json d):data(d){};
 
 	json data;
-
+	operator bool() const {
+		return bool(data);
+	}
 };
 
 
 
-
 UCLASS()
-class UnlohmannjsonBPLibrary : public UBlueprintFunctionLibrary
+class NLOHMANNJSON_API UnlohmannjsonBPLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_UCLASS_BODY()
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Load JSON", Keywords = "nlohmannjson"), Category = "nlohmannjson")
 	static FJSON loadjsonfile(const FString& file);
 
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Prase JSON", Keywords = "nlohmannjson"), Category = "nlohmannjson")
+	static FJSON Parse(const FString& content);
+
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Print JSON", Keywords = "nlohmannjson"), Category = "nlohmannjson")
 	static const FJSON& PrintJSON(const FJSON& JSON);
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Print JSON Type", Keywords = "nlohmannjson"), Category = "nlohmannjson")
 	static void PrintJSONType(const FJSON& JSON);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "ToString", Keywords = "nlohmannjson"), Category = "nlohmannjson")
+	static FString JSONToString(const FJSON& JSON);
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Get JSON String", Keywords = "nlohmannjson"), Category = "nlohmannjson")
 	static FString GetString(const FJSON& JSON, const FString& str);
@@ -50,14 +60,25 @@ class UnlohmannjsonBPLibrary : public UBlueprintFunctionLibrary
 	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Get JSON Boolean", Keywords = "nlohmannjson"), Category = "nlohmannjson")
 	static bool GetBoolean(const FJSON& JSON, const FString& str);
 	
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Is JSON Object Self", Keywords = "nlohmannjson"), Category = "nlohmannjson")
+	static bool IsObjectSelf(const FJSON& JSON);
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Is JSON Object", Keywords = "nlohmannjson"), Category = "nlohmannjson")
+	static bool IsObject(const FJSON& JSON, const FString& str);
 	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Get JSON Object", Keywords = "nlohmannjson"), Category = "nlohmannjson")
 	static FJSON GetObject(const FJSON& JSON, const FString& str);
 
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Is JSON Array Self", Keywords = "nlohmannjson"), Category = "nlohmannjson")
+	static bool IsArraySelf(const FJSON& JSON);
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Is JSON Array", Keywords = "nlohmannjson"), Category = "nlohmannjson")
+	static bool IsArray(const FJSON& JSON, const FString& str);
 	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Get JSON Array", Keywords = "nlohmannjson"), Category = "nlohmannjson")
 	static FJSON GetArray(const FJSON& JSON, const FString& str);
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Get JSON Array Element", Keywords = "nlohmannjson"), Category = "nlohmannjson")
 	static FJSON GetArrayElement(const FJSON& JSON, int32 Index);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "Get JSON Array Size", Keywords = "nlohmannjson"), Category = "nlohmannjson")
+	static int GetArraySize(const FJSON& JSON);
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "JSON To Value String", Keywords = "nlohmannjson"), Category = "nlohmannjson")
 	static FString ToString(const FJSON& JSON);
@@ -71,5 +92,11 @@ class UnlohmannjsonBPLibrary : public UBlueprintFunctionLibrary
 	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "JSON To Value Boolean", Keywords = "nlohmannjson"), Category = "nlohmannjson")
 	static bool ToBoolean(const FJSON& JSON);
 	
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "UrlEncode", Keywords = "nlohmannjson"), Category = "nlohmannjson")
+	static FString UrlEncode(const FString& String);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (DisplayName = "BinaryDecode", Keywords = "nlohmannjson"), Category = "nlohmannjson")
+	static FString BinaryDecode(const FString& Message);
+
 
 };
